2e30970ea4f43f481d3928276961cd14896de470
William Ruppenthal <williamruppenthal42@gmail.com>
2016-01-21 12:13:10 -0800

    V1.0 Fairly shit, but logs

--- src/MachineLearning.java
+++ src/MachineLearning.java
@@ -1,8 +1,10 @@
 /**
  * Created by WilliamRuppenthal on 1/11/16.
  */
+import java.nio.Buffer;
 import java.util.*;
 import java.io.*;
+import java.sql.Timestamp;
 
 enum outcome{
     WIN,DRAW,LOSS
@@ -22,6 +24,7 @@ enum status{
 
 public class MachineLearning{
     public List<Game> games=new ArrayList<Game>();
+    private double[] weights;
 
 
     public static void main(String[] args){
@@ -35,7 +38,76 @@ public class MachineLearning{
                 c++;
             d++;
         }
-        System.out.println((double)c/(double)d);
+
+        boolean[] results=new boolean[200];
+        for(int i=0;i<1000;i++)
+            if(i<100)
+                results[i]=m.learn(i);
+            else if(i>899)
+                results[i-800]=m.learn(i);
+            else
+                m.learn(i);
+
+        int earlyp=0,latep=0;    //early and late percentage correct, respectively
+        int right=0;    //Number of games right
+        for(int i=0;i<100;i++)
+            if(results[i])
+                right++;
+        earlyp=right;
+
+        right=0;
+        for(int i=100;i<200;i++)
+            if(results[i])
+                right++;
+        latep=right;
+
+        System.out.println("Percentage right in first 100: "+earlyp);
+        System.out.println("Percentage right in last 100: "+latep);
+
+        m.logData(earlyp,latep);
+    }
+
+    public MachineLearning(){
+        weights=new double[42];
+        for(int c=0;c<weights.length;c++)
+            weights[c]=1;
+    }
+
+    private boolean learn(int i){
+        double score=0;
+        status[] stats=this.games.get(i).getArr();
+        outcome o,actual=this.games.get(i).getOutcome();
+
+        for(int c=0;c<weights.length;c++)
+            if(stats[c]==status.X)
+                score+=this.weights[c];
+            else if(stats[c]==status.O)
+                score-=this.weights[c];
+
+        if(score>.3)
+            o=outcome.WIN;
+        else if(score>-.3)
+            o=outcome.DRAW;
+        else
+            o=outcome.LOSS;
+
+        if(o!=actual){
+            for(int c=0;c<this.weights.length;c++){
+                if(actual==outcome.WIN)
+                    if(stats[c]==status.X)
+                        weights[c]+=1/Math.sqrt(i);
+                    else if(stats[c]==status.O)
+                        weights[c]-=1/Math.sqrt(i);
+                else if(actual==outcome.LOSS)
+                    if(stats[c]==status.X)
+                        weights[c]-=1/Math.sqrt(i);
+                    else if(stats[c]==status.O)
+                        weights[c]+=1/Math.sqrt(i);
+            }
+
+            return false;
+        }
+        return true;
     }
 
     private void readData(String filename){
@@ -65,5 +137,24 @@ public class MachineLearning{
             }
         }catch(FileNotFoundException e){}
     }
-}
 
+    private void logData(int earlyp,int latep){
+        File f=new File("log.txt");
+        String s=(new Timestamp(System.currentTimeMillis())).toString()
+                +"\n"+earlyp+"\n"+latep;
+        try{
+            if(!f.exists())
+                f.createNewFile();
+
+            FileWriter fw=new FileWriter(f,true);
+            BufferedWriter bw=new BufferedWriter(fw);
+            PrintWriter out=new PrintWriter(bw);
+
+            out.println(s);
+
+            out.close();
+        }catch(Exception e){
+            System.out.println("Failed to log");
+        }
+    }
+}
